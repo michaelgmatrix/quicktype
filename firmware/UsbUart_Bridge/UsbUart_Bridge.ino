3f17950ad669ab4f034846d711043166cd1bbf95
michaelmatrix <michael.g.matrix@gmail.com>
2026-07-25 18:05:45 -0700

    Restore Native USB Host Controller 0 on UsbUart_Bridge

--- firmware/UsbUart_Bridge/UsbUart_Bridge.ino
+++ firmware/UsbUart_Bridge/UsbUart_Bridge.ino
@@ -2,14 +2,12 @@
 #include <Arduino.h>
 #include "QuickTypeUartProtocol.h"
 
-#include <pio_usb.h>
 #include <Adafruit_TinyUSB.h>
 
 #ifndef USE_TINYUSB
 #error This keyboard-facing firmware requires the RP2040 native TinyUSB stack.
 #endif
 
-static constexpr uint8_t USB_HOST_DP_GPIO = 0;
 static constexpr uint8_t UART_TX_PIN = 4;
 static constexpr uint8_t UART_RX_PIN = 5;
 static constexpr uint32_t UART_BAUD = 1000000;
@@ -164,15 +162,12 @@ void setup() {
   Serial2.begin(UART_BAUD);
   writePacket(QuickTypeUart::TYPE_HEARTBEAT, nullptr, 0);
 
-  pio_usb_configuration_t pioConfig = PIO_USB_DEFAULT_CONFIG;
-  pioConfig.pin_dp = USB_HOST_DP_GPIO;
   tuh_hid_set_default_protocol(HID_PROTOCOL_REPORT);
-  USBHost.configure_pio_usb(1, &pioConfig);
-  USBHost.begin(1);
+  USBHost.begin(0);
 }
 
 void loop() {
-  USBHost.task();
+  USBHost.task(0);
 
   uint32_t now = millis();
   if (now - lastHeartbeatMs >= HEARTBEAT_INTERVAL_MS) {
